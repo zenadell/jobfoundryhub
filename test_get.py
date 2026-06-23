b7ba6e3ef32b7bae889cb30611fd93bc547b9044
MAC <mac@Powers-MacBook-Pro.local>
2026-06-23 09:18:09 +0100

    chore: clean up test files

--- test_get.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
-django.setup()
-
-from apps.blog.models import Post
-print(f"Posts count: {Post.objects.count()}")
